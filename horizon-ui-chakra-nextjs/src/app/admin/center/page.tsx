ed1015f39011d3538195e00b54ad5ea9015be6e6
RadwaaAyman <radwa.ayman@totplatform.com>
2024-07-01 17:23:45 +0300

    enhance in add center button

--- horizon-ui-chakra-nextjs/src/app/admin/center/page.tsx
+++ horizon-ui-chakra-nextjs/src/app/admin/center/page.tsx
@@ -9,12 +9,15 @@ import CompactTable from 'components/common/compact-table/CompactTable';
 import { getCenter } from 'libs/endpoints/center';
 
 const Page = () => {
+
+  const textColor = useColorModeValue('secondaryGray.900', 'white');
+  const router = useRouter();
   const [Centers, setCenters] = useState<{
     headers: Array<{ title: string; field: string }>;
     data?: Array<any>;
   }>();
-  const textColor = useColorModeValue('secondaryGray.900', 'white');
-  const router = useRouter();
+   const [isDisabled,setIsDisabled] = useState<boolean>(false)
+
 
   const handleOnEdit = async (id: number) => {
     await getCenter();
@@ -22,26 +25,28 @@ const Page = () => {
   };
 
 
-  const loadData = useCallback(() => {
-    getCenter().then((data: any) => {
-      if (data) {
-        setCenters((prev) => ({
-          headers: [
-            {title: "Name", field: "centerName"},
-            {title: "Opening Hours", field: "openingHours"},
-            {title: "Specialty", field: "specialty"}          
-          ],
-          data: data,
-        }));
-      } else {
-        console.log('data not found');
-      }
+  const loadData = useCallback(async() => {
+    const data = await getCenter();
+    const hasData = Array.isArray(data) && data.length > 0;
+
+    setCenters({
+      headers: [
+        { title: 'Name', field: 'centerName' },
+        { title: 'Opening Hours', field: 'openingHours' },
+        { title: 'Specialty', field: 'specialty' },
+      ],
+      data: hasData ? data : [],
     });
+
+    setIsDisabled(hasData);
   }, []);
+
 
   useEffect(() => {
     loadData();
   }, [loadData]);
+
+
   return (
     <Card
       flexDirection="column"
@@ -67,7 +72,7 @@ const Page = () => {
             <button
               type="button"
               style={{
-                backgroundColor: 'blue' /* Green background */,
+                backgroundColor: isDisabled ? 'dark blue' :  'blue' ,
                 border: 'none',
                 color: 'white',
                 padding: '10px 20px' /* Some padding */,
@@ -76,9 +81,10 @@ const Page = () => {
                 display: 'inline-block',
                 fontSize: '16px',
                 margin: '4px 2px',
-                cursor: 'pointer',
+                cursor: isDisabled ? 'not-allowed' : 'pointer',
                 borderRadius: '5px' /* Rounded corners */,
               }}
+              disabled={isDisabled}
             >
               Add new Center{' '}
               <span style={{ fontSize: '20px', fontWeight: 'bold' }}>+</span>
@@ -86,7 +92,7 @@ const Page = () => {
           </div>
         </Link>
 
-        {Centers && (
+        {Centers && Centers.data.length > 0 && (
           <CompactTable
             headers={Centers.headers}
             data={Centers.data}
